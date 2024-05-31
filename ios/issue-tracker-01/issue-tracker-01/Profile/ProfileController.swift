1ad07c7518365ec6d6320dcf0b13b2803e1e0575
joho2022 <josama1997@naver.com>
2024-05-31 13:20:27 +0900

    ✨ Feat(#84): 이미지뷰 gif 구현

--- ios/issue-tracker-01/issue-tracker-01/Profile/ProfileController.swift
+++ ios/issue-tracker-01/issue-tracker-01/Profile/ProfileController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ProfileController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var idLabel: UILabel!
     @IBOutlet weak var nicknameLabel: UILabel!
     let userProfile = UserProfileModel.shared.getUserProfile()
@@ -18,5 +19,28 @@ class ProfileController: UIViewController {
         
         idLabel.text = userProfile?.id
         nicknameLabel.text = userProfile?.nickname
+        imageView.layer.cornerRadius = 120
+        
+        setupConfigure()
+    }
+    
+    func setupConfigure() {
+        guard let gifURL = Bundle.main.url(forResource: "animation", withExtension: "gif"),
+              let gifData = try? Data(contentsOf: gifURL),
+              let source = CGImageSourceCreateWithData(gifData as CFData, nil) else { return }
+        
+        let frameCount = CGImageSourceGetCount(source)
+        var images = [UIImage]()
+        
+        for number in 0..<frameCount {
+            if let image = CGImageSourceCreateImageAtIndex(source, number, nil) {
+                images.append(UIImage(cgImage: image))
+            }
+        }
+        
+        imageView.animationImages = images
+        imageView.animationDuration = TimeInterval(frameCount) * 0.05
+        imageView.animationRepeatCount = 0
+        imageView.startAnimating()
     }
 }
